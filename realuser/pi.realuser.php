56712c6606288be382eceeb395c3663568673c22
Blis Web Agency <sam@blis.net.au>
2011-10-06 10:07:22 +1030

    Initial commit

---
+++ realuser/pi.realuser.php
@@ -0,0 +1,74 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * ExpressionEngine - by EllisLab
+ *
+ * @package		ExpressionEngine
+ * @author		ExpressionEngine Dev Team
+ * @copyright	Copyright (c) 2003 - 2011, EllisLab, Inc.
+ * @license		http://expressionengine.com/user_guide/license.html
+ * @link		http://expressionengine.com
+ * @since		Version 2.0
+ * @filesource
+ */
+ 
+// ------------------------------------------------------------------------
+
+/**
+ * RealUser Plugin
+ *
+ * @package		ExpressionEngine
+ * @subpackage	Addons
+ * @category	Plugin
+ * @author		Blis Web Agency
+ * @link		http://blis.net.au
+ */
+
+$plugin_info = array(
+	'pi_name'		=> 'RealUser',
+	'pi_version'	=> '1.0',
+	'pi_author'		=> 'Blis Web Agency',
+	'pi_author_url'	=> 'http://blis.net.au',
+	'pi_description'=> 'Checks if a user exists',
+	'pi_usage'		=> Realuser::usage()
+);
+
+
+class Realuser {
+
+	public $return_data;
+    
+	/**
+	 * Constructor
+	 */
+	public function __construct()
+	{
+		$this->EE =& get_instance();
+	}
+	
+	// ----------------------------------------------------------------
+	
+	/**
+	 * Plugin Usage
+	 */
+	public static function usage()
+	{
+		ob_start();
+?>
+
+{exp:realuser:check username="admin"}
+
+This will output: true (if the username is found)
+or: false (if the username isn't found)
+
+Useful for AJAX
+<?php
+		$buffer = ob_get_contents();
+		ob_end_clean();
+		return $buffer;
+	}
+}
+
+
+/* End of file pi.realuser.php */
+/* Location: /system/expressionengine/third_party/realuser/pi.realuser.php */
